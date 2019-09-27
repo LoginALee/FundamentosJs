23ec09a5b1ed1b001929dd971bc537f80284e7dc
Alejandro Tinoco <Alejandrodxwwe@hotmail.com>
2019-09-27 12:59:30 -0500

    Clase 2

---
+++ clase2.js
@@ -0,0 +1,13 @@
+var nombre = 'Sacha';
+var apellido = 'Morales';
+
+var nombreEnMayusculas = nombre.toUpperCase();
+var apellidoEnMinusculas = apellido.toLowerCase();
+var primeraLetraDelNombre = nombre.charAt(0);
+var cantidadDeLetrasDelNombre = nombre.length;
+
+var nombreCompleto = `${nombre} ${apellido.toUpperCase()}`;
+
+var str = nombre.substr(1,2);
+
+var ultimaLetra = nombre.charAt(nombre.length - 1);
