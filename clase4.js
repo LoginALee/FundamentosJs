2566c6cbd0d21b3c6b0543fddea8880cdb1dbd51
Alejandro Tinoco <Alejandrodxwwe@hotmail.com>
2019-09-28 12:48:42 -0500

    Clase 4

---
+++ clase4.js
@@ -0,0 +1,11 @@
+var nombre = 'Sacha';
+var edad = 28;
+
+function imprimirEdad(n, e){
+    console.log(`${n} tiene ${e} años`);
+}
+
+imprimirEdad(nombre, edad);
+imprimirEdad('Vicky', 19);
+imprimirEdad('Eric', 29);
+imprimirEdad(20, 'Miguel');
