4750a59df5b3107eb1fe43615096efd92537fc65
Alejandro Tinoco <Alejandrodxwwe@hotmail.com>
2019-09-30 21:08:05 -0500

    Clase 16

---
+++ clase16.js
@@ -0,0 +1,32 @@
+var signoZodiacal = prompt('¿Cual es tu signo?');
+signoZodiacal = signoZodiacal.toLowerCase();
+
+
+switch(signoZodiacal){
+    case 'acuario':
+        console.log('Los acuario son aksdjkasd');
+        break;
+    case 'piscis':
+        console.log('Los piscis son aksdjkasd');
+        break;
+    case 'cancer':
+        console.log('Los cancer son aksdjkasd');
+        break;  
+    case 'libra':
+        console.log('Los libra son aksdjkasd');
+        break;
+    case 'escorpio':
+        console.log('Los escorpio son aksdjkasd');
+        break;
+    case 'sagitario':
+        console.log('Los sagitario son aksdjkasd');
+        break;
+    case 'capricornio':
+        console.log('Los capricornio son aksdjkasd');
+        break;
+    case 'tauro':
+        console.log('Los tauro son aksdjkasd');
+        break;  
+    default:
+        console.log('Escribe un signo existente');
+}
