36e95e4e4a8fa2c9f8008402ecfd5fe61f1a637d
Alejandro Tinoco <Alejandrodxwwe@hotmail.com>
2019-10-01 20:50:08 -0500

    Clase 18

---
+++ clase18.js
@@ -0,0 +1,54 @@
+var alan = {
+    nombre: 'Alan',
+    apellido: 'Perez',
+    altura: 1.80
+}
+
+var vicky = {
+    nombre: 'Vicky',
+    apellido: 'Morales',
+    altura: 1.70
+}
+
+var miguel = {
+    nombre: 'Miguel',
+    apellido: 'Nogales',
+    altura: 1.82
+}
+
+var alejandro = {
+    nombre: 'Alejandro',
+    apellido: 'Tinoco',
+    altura: 1.85
+}
+
+var maria = {
+    nombre: 'Maria',
+    apellido: 'DB',
+    altura: 1.40
+}
+
+var lupita = {
+    nombre: 'Lupita',
+    apellido: 'Cañas',
+    altura: 1.60
+}
+
+var personas = [lupita, maria, alejandro, miguel, vicky, alan];
+
+// for(var i = 0; i < personas.length; i++){
+//     var persona = personas[i];
+//     console.log(`${persona.nombre} mide ${persona.altura}mts`)
+// }
+
+for (let persona of personas){
+    console.log(`${persona.nombre} mide ${persona.altura.toFixed(2)} mts`);
+}
+
+const esAlta = ({ altura }) => {
+    return altura > 1.8;
+};
+
+var personasAltas = personas.filter(esAlta);
+
+console.log(personasAltas);
