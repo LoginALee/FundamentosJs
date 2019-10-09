13a52e9c50b6b4f705bb952ee6e9322fd976400d
Alejandro Tinoco <Alejandrodxwwe@hotmail.com>
2019-10-08 20:17:49 -0500

    clase 30

---
+++ clase30.js
@@ -0,0 +1,25 @@
+const API_URL = 'https://swapi.co/api/';
+const PEOPLE_URL = 'people/:id';
+const OPTS = { crossDomain: true };
+
+
+
+function obtenerPersonaje(id, callback){
+    const URL = `${API_URL}${PEOPLE_URL.replace(':id', id)}`
+    
+    $.get(URL, OPTS, function(persona) {
+        console.log(`Hola, soy ${persona.name}`)
+    });
+
+    if(callback){
+        callback();
+    }
+};
+
+obtenerPersonaje(1, function(){
+    obtenerPersonaje(2, function(){
+        obtenerPersonaje(3);
+    });
+});
+
+
