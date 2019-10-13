4b608bdfb944b43d21717df9e40427e8193870ed
Alejandro Tinoco <Alejandrodxwwe@hotmail.com>
2019-10-13 14:40:57 -0500

    Clase 34

---
+++ clase34.js
@@ -0,0 +1,77 @@
+const API_URL = 'https://swapi.co/api/';
+const PEOPLE_URL = 'people/:id';
+const OPTS = { crossDomain: true };
+
+
+
+function obtenerPersonaje(id){
+    return new Promise((resolve, reject) =>{
+        const URL = `${API_URL}${PEOPLE_URL.replace(':id', id)}`
+
+        
+        $
+        .get(URL, OPTS, function(data){
+            resolve(data)
+        })
+
+        .fail(()=> reject(id))
+    })
+};
+
+function onError(id){
+    console.log(`Sucedio un error al obtener el personaje ${id}`)
+};
+
+// var ids = [1, 2, 3, 4, 5, 6, 7];
+// var promesas = ids.map(function(id){
+//     return obtenerPersonaje(id)
+// });
+
+var ids = [1, 2, 3, 4, 5, 6, 7];
+
+var promesas = ids.map(id => {
+    return obtenerPersonaje(id)
+});
+
+Promise
+    .all(promesas)
+    .then(personajes => console.log(personajes))
+    .catch(onError)
+
+
+
+    // obtenerPersonaje(1)
+    //     .then(persona => {
+    //         console.log(`El personaje 1 es ${persona.name}`);
+    //         return obtenerPersonaje(2);
+    //     })
+    //     .then(persona => {
+    //         console.log(`El personaje 2 es ${persona.name}`);
+    //         return obtenerPersonaje(3);
+    //     })
+    //     .then(persona => {
+    //         console.log(`El personaje 3 es ${persona.name}`);
+    //         return obtenerPersonaje(4);
+    //     })
+    //     .then(persona => {
+    //         console.log(`El personaje 4 es ${persona.name}`);
+    //         return obtenerPersonaje(5);
+    //     })
+    //     .then(persona => {
+    //         console.log(`El personaje 5 es ${persona.name}`);
+    //         return obtenerPersonaje(6);
+    //     })
+    //     .then(persona => {
+    //         console.log(`El personaje 6 es ${persona.name}`);
+    //         return obtenerPersonaje(7);
+    //     })
+    //     .then(persona => {
+    //         console.log(`El personaje 7 es ${persona.name}`);
+    //     })
+    //     .catch(onError)
+
+
+
+    
+
+
