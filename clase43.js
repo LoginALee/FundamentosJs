9c740f4a57691481b5d252726536c9a3af93940c
Alejandro Tinoco <Alejandrodxwwe@hotmail.com>
2019-10-18 13:03:32 -0500

    Clase 43

---
+++ clase43.js
@@ -0,0 +1,25 @@
+//Opcional
+alert('Hola');
+var nombre = 'Ale';
+
+//Innecesario 100%
+const saludar = function(){
+
+}
+
+for(var i = 0; i < 10; i++){
+}
+
+//Necesario
+console.log('Holaskjkajdkasjd'); // <----
+[1, 2, 3].forEach(n => console.log(n * 2));
+
+const nombre = 'Ale'
+console.log('Hola kjasjaks'); // <----
+`${nombre} es ukajdkajd`
+
+function calcularDoble(numero){
+    return{
+        original: numero, doble: numero * 2
+    }
+}
